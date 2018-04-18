fe91f8363c11a4ca4552bcca8f9b528eabc40432
Jaakko <jaakko@kronogra.fi>
2018-04-18 19:20:01 +0300

    changes

--- Blog/myscript.js
+++ Blog/myscript.js
@@ -6,21 +6,25 @@ function setup(){
 }
 
 function clicked(){
-    var json;
-    loadJSON(function(response){
-        json = JSON.parse(response);
-    });
+    var json; 
+    loadJSON(function(response) {
+        // Parse JSON string into object
+          var json = JSON.parse(response);
+       });
     alert(json.users[0].name);
 }
 
-function loadJSON(callback){
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'data.json', true);
-    xobj.onreadystatechange = function(){
-        if(xobj.readyState == 4 && xobj.status == "200"){
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
+function loadJSON(callback) {   
+    
+        var xobj = new XMLHttpRequest();
+            xobj.overrideMimeType("application/json");
+        xobj.open('GET', 'data.json', true); // Replace 'my_data' with the path to your file
+        xobj.onreadystatechange = function () {
+              if (xobj.readyState == 4 && xobj.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(xobj.responseText);
+              }
+        };
+        xobj.send(null);  
+     }
+     
